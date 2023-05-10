0e93919cf93c8aa9343ec64af5264fbffe3d114f
Nicholas De Pontes <nicholas.depontes@sky.uk>
2023-05-10 14:14:39 +0100

    fix

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftFortuneWheel",
     platforms: [
-        .iOS(.v9), .tvOS(.v9), .macOS(.v10_11)
+        .iOS(.v13), .tvOS(.v13), .macOS(.v12)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
